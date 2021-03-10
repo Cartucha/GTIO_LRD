37956211c383fe96b568ca8a02df2fe350bbebc4
ujuetarrika94gmail.com <ujuetarrika94gmail.com>
2021-03-10 17:49:15 +0100

    Actualizar proxy

---
+++ src/proxyup/proxy.js
@@ -0,0 +1,51 @@
+"use strict"
+// Librerias
+const zmq = require("zmq");
+
+if (process.argv.length != 4) {
+	console.log("Uso: " + process.argv[1] + " <ipF:puertoF> <ipB:puertoB>");
+	process.exit(-1);
+}
+
+let hostF = process.argv[2];
+let hostB = process.argv[3];
+
+let frontend = zmq.socket("router"); // socket para conexion con clientes
+let backend = zmq.socket("router"); // socket para conexion con manejadores
+frontend.identity = "frontend";
+backend.identity = "backend";
+
+// Conectar
+frontend.bind("tcp://" + hostF);
+backend.bind("tcp://" + hostB);
+
+let ids = [];
+
+// Reaccion en frontend
+frontend.on("message", (fuente, _, message) => {
+	// Reaccionar
+	let msg = JSON.parse(message.toString());
+	backend.send([msg.dest, "", JSON.stringify(msg)]);
+	console.log(msg);
+});
+
+// Reaccion en backend
+backend.on("message", (fuente, _, message) => {
+	// Comprobar si es mensaje de presentacion
+	if (ids[fuente] == undefined) {
+		console.log("new handler " + fuente);
+		ids[fuente] = {};
+	} else {
+		// Reaccionar
+		let msg = JSON.parse(message.toString());
+		frontend.send([msg.dest, "", JSON.stringify(msg)]);
+		console.log(msg);
+	}
+});
+
+// El proceso se interrumpe
+process.on("SIGINT", () => {
+	frontend.close();
+	backend.close();
+	process.exit(-1);
+});
